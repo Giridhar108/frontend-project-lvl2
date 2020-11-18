2c70ce291f387fd317b3fe8aaad33ae74edaf637
German Fomenko <pentagram777@mail.ru>
2020-11-18 12:05:45 +0500

    feat diff logic

--- src/genDiff.js
+++ src/genDiff.js
@@ -1,14 +1,30 @@
-import * as fs from'fs';
-import * as path from'path';
+import * as fs from "fs";
+import * as path from "path";
 export default (pathOne, pathTwo) => {
-  // console.log(pathOne, pathTwo)
-    const first = fs.readFileSync(path.resolve(`${pathOne}`));
-    const second = fs.readFileSync(path.resolve(`${pathTwo}`));
-    // console.log(data.toString())
-    console.log(JSON.parse(first))
-    console.log(JSON.parse(second))
-    console.log(path.resolve(`${pathTwo}`))
-}
+  const first = JSON.parse(fs.readFileSync(path.resolve(`${pathOne}`)));
+  const second = JSON.parse(fs.readFileSync(path.resolve(`${pathTwo}`)));
+
+  const keys = Object.entries({ ...first, ...second }).sort();
+
+  return JSON.stringify(keys.reduce((acc, c) => {
+    if (first[`${c[0]}`] === second[`${c[0]}`]) {
+      acc[`${c[0]}`] = c[1];
+    } else if (
+      first[`${c[0]}`] !== second[`${c[0]}`] &&
+      first[`${c[0]}`] !== undefined &&
+      second[`${c[0]}`] !== undefined
+    ) {
+      acc[`- ${c[0]}`] = first[`${c[0]}`];
+      acc[`+ ${c[0]}`] = second[`${c[0]}`];
+    } else if (first[`${c[0]}`] === undefined) {
+      acc[`+ ${c[0]}`] = second[`${c[0]}`];
+    } else if (second[`${c[0]}`] === undefined) {
+      acc[`- ${c[0]}`] = first[`${c[0]}`];
+    }
+
+    return acc;
+  }, {}));
+};
 
 /* 
 c:/hexlet-git/frontend-project-lvl2/path/first.json
@@ -18,4 +34,3 @@ c:/hexlet-git/frontend-project-lvl2/path/second.json
  ../frontend-project-lvl2/path/first.json
  ../frontend-project-lvl2/path/second.json
  */
-
