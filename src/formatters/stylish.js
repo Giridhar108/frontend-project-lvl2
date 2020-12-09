1ae55293745fd7ef1fc63240b87632b2cee12fd3
German Fomenko <pentagram777@mail.ru>
2020-12-09 14:50:29 +0500

    fix stylish space

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -7,24 +7,27 @@ export default (obj, r = '  ', s = 2) => {
         if (typeof (value) === 'object' && value !== null) {
           if (!Array.isArray(value)) {
             const some = Object.entries(value);
-            return `{${iter(some, depth + 1)}\n${repeat}  }`;
+            return ` {${iter(some, depth + 1)}\n${repeat}  }`;
           }
-          return `{${iter(value, depth + 1)}\n${repeat}  }`;
+          return ` {${iter(value, depth + 1)}\n${repeat}  }`;
         }
-        return `${value}`;
+        if (value === '') {
+          return `${value}`;
+        }
+        return ` ${value}`;
       };
       if (key.status === 'hasChildren') {
-        return `\n${repeat}  ${key.name}: ${stringify(key.children)}`;
+        return `\n${repeat}  ${key.name}:${stringify(key.children)}`;
       } if (key.status === 'add') {
-        return `\n${repeat}+ ${key.name}: ${stringify(key.value)}`;
+        return `\n${repeat}+ ${key.name}:${stringify(key.value)}`;
       } if (key.status === 'was') {
-        return `\n${repeat}  ${key.name}: ${stringify(key.value)}`;
+        return `\n${repeat}  ${key.name}:${stringify(key.value)}`;
       } if (key.status === 'deleted') {
-        return `\n${repeat}- ${key.name}: ${stringify(key.value)}`;
+        return `\n${repeat}- ${key.name}:${stringify(key.value)}`;
       } if (key.status === 'change') {
-        return `\n${repeat}- ${key.name}: ${stringify(key.oldValue)}\n${repeat}+ ${key.name}: ${stringify(key.newValue)}`;
+        return `\n${repeat}- ${key.name}:${stringify(key.oldValue)}\n${repeat}+ ${key.name}:${stringify(key.newValue)}`;
       }
-      return `\n${repeat}  ${key[0]}: ${stringify(key[1])}`;
+      return `\n${repeat}  ${key[0]}:${stringify(key[1])}`;
     });
   };
   return `{${iter(obj, 1).join().replace(/,/g, '')}\n}`;
