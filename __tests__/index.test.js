9b4f1e3870f630cc5a62628c75965df8b69e3e21
German Fomenko <pentagram777@mail.ru>
2020-12-22 14:15:27 +0500

    fix name test

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -4,44 +4,39 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 import genDiff from '../src/index.js';
 
-const filename = fileURLToPath(import.meta.url);
-const dirname = path.dirname(filename);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-const getFixturePath = (filename) => path.join(dirname, '..', '__fixtures__', filename);
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
 const getData = (fileName) => fs.readFileSync(getFixturePath(fileName), 'utf-8');
 
-test('test JSON big', () => {
+test('JSON big', () => {
   expect(genDiff(getFixturePath('first.json'), getFixturePath('second.json')))
     .toBe(getData('expectedStylish.txt'));
 });
 
-test('test JSON big stylish', () => {
+test('JSON big stylish', () => {
   expect(genDiff(getFixturePath('first.json'), getFixturePath('second.json'), 'stylish'))
     .toBe(getData('expectedStylish.txt'));
 });
 
-test('test JSON big plain', () => {
+test('JSON big plain', () => {
   expect(genDiff(getFixturePath('first.json'), getFixturePath('second.json'), 'plain'))
     .toBe(getData('expectedPlain.txt'));
 });
 
-test('test YAML big', () => {
+test('YAML big', () => {
   expect(genDiff(getFixturePath('first.yaml'), getFixturePath('second.yaml')))
     .toBe(getData('expectedStylish.txt'));
 });
 
-test('test JSON big stylish', () => {
-  expect(genDiff(getFixturePath('first.json'), getFixturePath('second.json'), 'stylish'))
-    .toBe(getData('expectedStylish.txt'));
-});
-
-test('test YAML big plain', () => {
+test('YAML big plain', () => {
   expect(genDiff(getFixturePath('first.yaml'), getFixturePath('second.yaml'), 'plain'))
     .toBe(getData('expectedPlain.txt'));
 });
 
-test('test YAML big json', () => {
+test('YAML big json', () => {
   expect(genDiff(getFixturePath('first.yaml'), getFixturePath('second.yaml'), 'json'))
     .toBe(getData('expectedJSON.txt'));
 });
